797b8a17e8a1296b91344153b3e0f5f4720fb6b1
PaulakaPaul <p.e.iusztin@gmail.com>
2018-08-17 18:18:33 +0300

    Added first functions

---
+++ croom-create/create_room.js
@@ -0,0 +1,33 @@
+var RedisSMQ = require("rsmq");
+var settings = require('./common/settings')
+var rsmq = new RedisSMQ(settings.REDIS_OPTIONS)
+
+exports.handler = function(e, ctx, callback) {
+
+    let listener = e.listener;
+    let users = e.users;
+
+    if(listener == undefined || users == undefined) {
+        callback(new Error("There are no listener or users passed as parameters"));
+        rsmq.quit();
+    } else {
+
+        let cr_namespace = `${settings.CHAT_ROOM_NAMESPACE}-${listener}`;        
+        users.forEach( (user, index) => {
+            let cr_queue_namespace = `${cr_namespace}-${user}`; // Per user chat queue.
+
+            rsmq.createQueue( {qname: cr_queue_namespace}, function (err, resp) {
+
+                if(err) {
+                    callback(err);
+                }
+
+                if (resp===1 && users.length - 1 == index) {
+                    rsmq.quit(); // After creating the last queue close the redis client.
+                }
+            });
+        });
+
+        callback(null, `Queues for chatroom ${cr_namespace} were created successfully`);
+    }
+}
